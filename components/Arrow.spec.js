06ab18d7c9c3c20cdddb3427c32acf359d856b34
Vannsl <boehner.vanessa@gmail.com>
2019-01-27 11:53:21 +0100

    adds complete test file for Arrow.vue

--- components/Arrow.spec.js
+++ components/Arrow.spec.js
@@ -11,4 +11,22 @@ describe("Arrow", () => {
   it("renders the correct markup", () => {
     expect(wrapper).toMatchSnapshot();
   });
+
+  describe("when the direction is up", () => {
+    it("sets the margin top to 7px", () => {
+      expect(wrapper.vm.marginTop).toBe("7px");
+    });
+  });
+
+  describe("when the direction is any other than up", () => {
+    beforeEach(() => {
+      wrapper.setProps({
+        direction: "down",
+      });
+    });
+
+    it("sets the margin top to 7px", () => {
+      expect(wrapper.vm.marginTop).toBe("");
+    });
+  });
 });
