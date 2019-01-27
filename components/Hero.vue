5037c53eeef746cde70723cd08ce79c2eb9331cb
Vannsl <boehner.vanessa@gmail.com>
2019-01-27 15:01:01 +0100

    changes hero image

--- components/Hero.vue
+++ components/Hero.vue
@@ -10,7 +10,7 @@ export default {
 
 <style lang="scss" scoped>
 .hero {
-  background: url("https://res.cloudinary.com/vannsl-io/image/upload/c_scale,w_2400,q_40/v1542542993/hero.jpg");
+  background: url("https://res.cloudinary.com/vannsl-io/image/upload/c_scale,w_2400,q_40/v1548597279/IMG_7750.jpg");
   background-position: center;
   background-size: cover;
   min-height: calc(100vh - 0px);
