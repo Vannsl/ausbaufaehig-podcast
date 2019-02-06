6e76ea6c565f957f2dae7a5be7f2bf55918fe959
Vannsl <boehner.vanessa@gmail.com>
2019-02-06 15:43:51 +0100

    adds defer keyword to google font

--- nuxt.config.js
+++ nuxt.config.js
@@ -54,7 +54,7 @@ export default {
     link: [
       { rel: "icon", type: "image/x-icon", href: "/favicon.png" },
       {
-        defer,
+        defer: true,
         rel: "stylesheet",
         href: "https://fonts.googleapis.com/css?family=PT+Sans+Narrow"
       }
